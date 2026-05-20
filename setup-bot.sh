2f8ba975340bdaf904486632859bf09c4605ad5f
AlpasNet <117809035+AlpasNet@users.noreply.github.com>
2026-05-20 18:12:50 +0200

    Add 'he' package to npm install command

--- setup-bot.sh
+++ setup-bot.sh
@@ -4,4 +4,4 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt install nodejs npm
 npm init -y
-npm install discord.js cheerio node-fetch dotenv
+npm install discord.js cheerio node-fetch dotenv he
